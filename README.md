ca16ac49b621a569e822f36929a2940c7116e708
Gyula Madarasz <gyula.madarasz@gmail.com>
2020-11-25 20:01:39 +0100

    Refactoring boilerplate

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+composer update
+vendor/bin/phan --init --init-level=1
+./test.sh
+```
--- index.php
+++ index.php
@@ -1,13 +1,13 @@
-<?php
+<?php declare(strict_types = 1);
 
-namespace Madsoft\Talkbot;
+namespace Madsoft\Boilerplate;
 
 use Madsoft\Library\App;
 use Madsoft\Library\Invoker;
 use Madsoft\Library\Responder\Account\AccountConfig;
 use Madsoft\Library\Responder\Example\Example;
 
-include __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
@@ -16,9 +16,9 @@ error_reporting(E_ALL | E_STRICT);
 $invoker = isset($this) && isset($this->invoker) ? $this->invoker : new Invoker();
 $output = (new App($invoker))->getOutput(
     [
-        AccountConfig::ROUTES, 
+        AccountConfig::ROUTES,
         Example::ROUTES,
-        MyScripts::ROUTES,
+    //        MyScripts::ROUTES,
     ]
 );
 echo $output;
