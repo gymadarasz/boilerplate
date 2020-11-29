8e5897d0db094265512b3727e358d83860c115ae
Gyula Madarasz <gyula.madarasz@gmail.com>
2020-11-29 12:54:50 +0100

    Distro files added

---
+++ dist/index.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types = 1);
+
+namespace Madsoft\Boilerplate;
+
+use Madsoft\Library\App;
+use Madsoft\Library\Invoker;
+use Madsoft\Library\Responder\Account\AccountConfig;
+use Madsoft\Library\Responder\Example\Example;
+
+require __DIR__ . '/vendor/autoload.php';
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL | E_STRICT);
+
+$invoker = isset($this) && isset($this->invoker) ? $this->invoker : new Invoker();
+$output = (new App($invoker))->getOutput(
+    [
+        AccountConfig::ROUTES,
+        Example::ROUTES,
+    //        MyScripts::ROUTES,
+    ]
+);
+echo $output;
